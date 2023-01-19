82b057fa357ad12e5504d15080a6fa47538bed56
Tina Tram <tinatram07@gmail.com>
2023-01-18 21:43:55 -0600

    added two images to images folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/balance.jpg', description: 'Photo of rocks of various sizes being stacked on top of each other.', likes: 0 },
+    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/read_coffee.jpg', description: 'Photo of an open book next to a cup of coffee on a wooden table.', likes: 0 }
 ];
 
 module.exports = galleryItems;
