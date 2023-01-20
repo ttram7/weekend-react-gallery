dabba2c19e66ff00f03b91fac0660f6e1c09c35e
Tina Tram <tinatram07@gmail.com>
2023-01-20 16:22:11 -0600

    created conditional in toggleDisplay function to set img class to opposite class to appear/not appear, created getter and setter for likeCount, added button for likes

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,19 +4,31 @@ import './/GalleryItem.css';
 function GalleryItem({photo}) {
     const [cssClass, setCssClass] = useState('visible');
     const [display, setDisplay] = useState(true);
+    const [likeCount, setLikeCount] = useState(0);
     //toggle setDisplay -> true/false on/off
 
     const toggleDisplay = () => {
         console.log('in toggleDisplay')
-        setDisplay(!display)
-        console.log(display)
-        //error: stops after false; react doesn't work with visibility
-        if (display == true) {
-            setCssClass('visible')
-        } else {
+        if (cssClass === 'visible') {
             setCssClass('hidden')
+            // return(
+            // <p>test description</p>
+            //     )
+            showDescription();
+        } else {
+            setCssClass('visible')
         }
-        console.log(cssClass)
+        // setCssClass('hidden')
+        // setDisplay(!display)
+    
+        //error: stops after false; react doesn't work with visibility
+        // if (display == true) {
+        //     setCssClass('visible')
+        // } else {
+        //     setCssClass('hidden')
+        // }
+        
+
     }
     // else, else if
 
@@ -31,10 +43,12 @@ function GalleryItem({photo}) {
 
     //  if showDisplay is true -> show description
 
-    const showDescription = (photo) => {
-        photo.display = 'none';
-        <p>{photo.description}</p>
-        console.log(photo.description);
+    const showDescription = () => {
+        <p>hi</p>
+        console.log('in showDiscription')
+    //     photo.display = 'none';
+    //     <p>{photo.description}</p>
+    //     console.log(photo.description);
     }
 
     return(
@@ -45,6 +59,10 @@ function GalleryItem({photo}) {
             onClick = {() => setDisplay('hidden')}></img>*/}
              <img src={photo.path} alt={photo.description} width="500" height="400" class={cssClass}
             onClick = {() => toggleDisplay()}></img>
+            <p class="centered">Center</p>
+            <button onClick = {() => setLikeCount(likeCount + 1)}>like it</button>
+            {likeCount}
+            
         </div>
     )
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,12 +1,12 @@
 import GalleryItem from '../GalleryItem/GalleryItem';
-
+import './/GalleryList.css'
 function GalleryList({list}) {
     return (
-        <>
+        <div class = 'gallery-grid'>
             {list.map(photo => 
                 (<GalleryItem photo={photo} />)
-            )}
-        </>
+            ) }
+        </div>
     )
 }
 export default GalleryList;
