12ea81fb194d4753adb591630c24293a268bc279
Tina Tram <tinatram07@gmail.com>
2023-01-20 22:37:25 -0600

    added conditional to display description if photo is hidden, imported GalleryItem css file, created divs for img and like-section, created put request to update like count

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,67 +1,50 @@
 import {useState} from 'react'
+import axios from 'axios';
 import './/GalleryItem.css';
 
 function GalleryItem({photo}) {
     const [cssClass, setCssClass] = useState('visible');
-    const [display, setDisplay] = useState(true);
     const [likeCount, setLikeCount] = useState(0);
-    //toggle setDisplay -> true/false on/off
+
 
     const toggleDisplay = () => {
         console.log('in toggleDisplay')
         if (cssClass === 'visible') {
             setCssClass('hidden')
-            // return(
-            // <p>test description</p>
-            //     )
-            showDescription();
         } else {
             setCssClass('visible')
         }
-        // setCssClass('hidden')
-        // setDisplay(!display)
-    
-        //error: stops after false; react doesn't work with visibility
-        // if (display == true) {
-        //     setCssClass('visible')
-        // } else {
-        //     setCssClass('hidden')
-        // }
-        
-
     }
-    // else, else if
 
-    
-
-    //const [description, setDescription];
-
-    // const showDescription = (description) => {
-    //     <p>{description}</p>
-    //     console.log(description);
-    // }
-
-    //  if showDisplay is true -> show description
-
-    const showDescription = () => {
-        <p>hi</p>
-        console.log('in showDiscription')
-    //     photo.display = 'none';
-    //     <p>{photo.description}</p>
-    //     console.log(photo.description);
+    //PUT request
+    const  increaseLikes = (id) => {
+        console.log('in increaseLikes');
+        
+        axios({
+            method: 'PUT',
+            url: `/gallery/like/${id}`,
+        })
+        .then((response) => {
+            console.log('PUT response from db: ', response)
+            //getPhotos();
+        })
+        .catch((error) => {
+            console.log('error making PUT request: ', error);
+        });
     }
 
     return(
         <div>
-            {/*<img src={photo.path} alt={photo.description} width="500" height="400"
-            onClick = {() => showDescription(photo.description)}></img>*/}
-            {/*<img src={photo.path} alt={photo.description} width="500" height="400" class={display}
-            onClick = {() => setDisplay('hidden')}></img>*/}
-             <img src={photo.path} alt={photo.description} width="500" height="400" class={cssClass}
-            onClick = {() => toggleDisplay()}></img>
-            <p class="centered">Center</p>
-            <button onClick = {() => setLikeCount(likeCount + 1)}>like it</button>
-            {likeCount}
+            <div class = 'indv-photo'>
+                <img src={photo.path} alt={photo.description} width="500" height="400" class={cssClass}
+                id = {photo.id} onClick = {() => toggleDisplay()}></img>
+                {cssClass === 'hidden' && <p class = "text">{photo.description}</p>}
+             </div>
+            <div class = 'like-section'>
+                <button onClick = {() => increaseLikes(photo.id)}>like it</button>
+                <p>{photo.likes} people like this</p>
+                {/*<button onClick = {() => setLikesCount(likes + 1)}>like it</button>*/}
+            </div>
             
         </div>
     )
